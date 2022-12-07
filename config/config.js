23d6b6b084e08ef10aa55a85b23001e2f6d571f0
Artyom Sankov <artemsankov22@gmail.com>
2022-12-07 20:01:35 +0300

    ssl add

--- config/config.js
+++ config/config.js
@@ -4,20 +4,35 @@ module.exports = development = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
   dialect: "postgres",
+  host: process.env.DATABASE_HOST,
+  dialectOptions: {
+    ssl: {
+      require: "true",
+    },
+  },
 };
 module.exports = test = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
   dialect: "postgres",
+  host: process.env.DATABASE_HOST,
+  dialectOptions: {
+    ssl: {
+      require: "true",
+    },
+  },
 };
 module.exports = production = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
   dialect: "postgres",
+  host: process.env.DATABASE_HOST,
+  dialectOptions: {
+    ssl: {
+      require: "true",
+    },
+  },
 };
